c2f2e46b66b605c448eccc3908bf91d3c3f598da
Naomi <gabriella.naomi21@ui.ac.id>
2025-03-24 14:51:50 +0700

    add sdk to env

--- config/ld.go
+++ config/ld.go
@@ -2,19 +2,31 @@ package config
 
 import (
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	ld "github.com/launchdarkly/go-server-sdk/v7"
 )
 
 var LDClient *ld.LDClient
 
+
 // Initiate LaunchDarkly Client
 func InitiateLDClient() {
-	// Ganti dengan SDK Key dari LaunchDarkly
-	sdkKey := "sdk-d4a84fa2-23f0-486b-af59-8985243ede13"
-	
-	// Inisialisasi LaunchDarkly Client
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found or failed to load")
+	}
+
+	// Ambil SDK Key dari environment variable
+	sdkKey := os.Getenv("SDK_LD")
+	if sdkKey == "" {
+		log.Fatal("Error: SDK_LD environment variable is missing")
+	}
+
+	// Inisialisasi LaunchDarkly Client dengan timeout 10 detik
 	client, err := ld.MakeClient(sdkKey, 10*time.Second)
 	if err != nil {
 		log.Fatalf("Failed to initialize LaunchDarkly client: %v", err)
@@ -22,8 +34,8 @@ func InitiateLDClient() {
 
 	// Simpan client ke variabel global
 	LDClient = client
+	log.Println("LaunchDarkly client successfully initialized")
 }
-
 // Fungsi untuk menutup LaunchDarkly Client saat aplikasi berhenti
 func CloseLDClient() {
 	if LDClient != nil {
