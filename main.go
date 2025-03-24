bb8ec78bada82ee0a71333f3007819ecfd5fc6b6
Naomi <gabriella.naomi21@ui.ac.id>
2025-03-24 17:54:48 +0700

    cors config

--- main.go
+++ main.go
@@ -5,17 +5,25 @@ import (
 	"github.com/gnh374/go-ld-demo/database"
 	"github.com/gnh374/go-ld-demo/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	// Enable CORS for all origins
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Mengizinkan semua origin
+		AllowMethods: "GET,POST,PUT,DELETE", // Mengizinkan metode HTTP tertentu
+		AllowHeaders: "Content-Type, Authorization", // Header yang diizinkan
+	}))
 
 	// Connect Database
 	database.ConnectDB()
 	database.MigrateDB()
 
 	config.InitiateLDClient()
-	defer config.CloseLDClient() 
+	defer config.CloseLDClient()
 
 	// Routes
 	app.Get("/users", handlers.GetUsers)
